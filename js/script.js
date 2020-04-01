af02302780e65d08ba602231519884f32a0736c9
Steven Ly <1stevenly92@gmail.com>
2020-04-01 16:32:03 -0700

    added logic and displayed it to html page

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
-
+   
     $("#form").submit(function(event){
         event.preventDefault();
+        var truthy = "true";
+        var falsey = "false"
+    
+
 
         var input = parseInt($("#input").val());
         console.log(input);
 
+        if ((input % 4 == 0) || (input % 400 == 0)){
+            console.log("true")
+            $("#result").text(truthy)
 
+
+
+        }else{
+            console.log("false");
+            $("#result").text(falsey)
+        }
 
 
 
