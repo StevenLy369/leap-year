bf709ac6dc5d3c2eff68a1674aff15bc42a2a0dc
Steven Ly <1stevenly92@gmail.com>
2020-04-01 16:22:07 -0700

    added inputs and console logged input

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $("#form").submit(function(event){
+        event.preventDefault();
+
+        var input = parseInt($("#input").val());
+        console.log(input);
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
